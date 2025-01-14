3cff897810ad4bf20893dec5a17866f0f04fb96e
fredy <fredy-andri.mottus@voco.ee>
2025-01-14 15:12:06 +0200

    create project base

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const db = require('./utils/db')
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<b>Page not found</b>')
+})
 
 app.get('/', (req, res) => {
     res.json({ message: 'web shop app'})

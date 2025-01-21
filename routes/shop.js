c76073608954ffdd6d56e6be8d62b3f6f14eb86c
Carl-Robert <robertm6ts@gmail.com>
2025-01-21 13:59:13 +0200

    css

--- routes/shop.js
+++ routes/shop.js
@@ -5,13 +5,13 @@ const ShopController = require('../controllers/shop')
 const router = express.Router();
 
 router.get('/', async (req, res, next) => ShopController.getIndex(req, res, next))
-//router.get('/products', async (req, res, next) => shopController.getAllProducts(req, res, next))
-//router.get('/products/;productId', async (req, res, next) => shopController.getProduct(req, res, next))
-//router.post('/products/:productId', async (req, res, next) => shopController.getProduct(req, res, next))
-//router.get('/cart', async (req, res, next) => shopController.getCart(req, res, next))
-//router.post('/cart', async (req, res, next) => shopController.postCart(req, res, next))
+router.get('/products', async (req, res, next) => shopController.getAllProducts(req, res, next))
+router.get('/products/;productId', async (req, res, next) => shopController.getProduct(req, res, next))
+router.post('/products/:productId', async (req, res, next) => shopController.getProduct(req, res, next))
+router.get('/cart', async (req, res, next) => shopController.getCart(req, res, next))
+router.post('/cart', async (req, res, next) => shopController.postCart(req, res, next))
 
-// router.post('/cart-delete-item', async (req, res, next) => shopController.cartDeleteitems(req, res, next))
+router.post('/cart-delete-item', async (req, res, next) => shopController.cartDeleteitems(req, res, next))
 
 
 

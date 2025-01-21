1a9edacb91e7371946c7823f31f018eafe4c33ef
Reinar Kajava <reinar.kajava@voco.ee>
2025-01-21 10:33:07 +0200

    WIP different things

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const fs = require('node:fs/promises');
+const shopController = require('../controllers/shop')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('<b>Web shop page</b>');
-});
+router.get('/', async (req, res, next) => shopController.getIndex(req, res, next))
+
+router.get('/products', async (req, res, next) => shopController.getAllProducts(req, res, next))
+
+router.get('/products/:productId', async (req, res, next) => shopController.getProduct(req, res, next))
+
+router.post('/products/:productId', async (req, res, next) => shopController.getProduct(req, res, next))
+
+router.get('/cart', async (req, res, next) => shopController.getCart(req, res, next))
+
+router.post('/cart', async (req, res, next) => shopController.getCart(req, res, next))
+router.post('/cart-delete-item', async (req, res, next) => shopController.cartDeleteItem(req, res, next))
 
 module.exports = router;

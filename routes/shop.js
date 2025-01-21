0c5bb885120c5421b3497fd16842073345c4bc86
Carl-Robert <robertm6ts@gmail.com>
2025-01-21 12:56:25 +0200

    Carts Database and full merge

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const fs = require('node:fs/promises')
-const shopController = require('../controllers/shop')
+const ShopController = require('../controllers/shop')
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => shopController.getIndex(req, res, next))
-router.get('/products', async (req, res, next) => shopController.getAllProducts(req, res, next))
-router.get('/products/;productId', async (req, res, next) => shopController.getProduc(req, res, next))
-router.post('/products/:productId', async (req, res, next) => shopController.getProduct(req, res, next))
-router.get('/cart', async (req, res, next) => shopController.getCart(req, res, next))
-router.post('/cart', async (req, res, next) => shopController.postCart(req, res, next))
+router.get('/', async (req, res, next) => ShopController.getIndex(req, res, next))
+//router.get('/products', async (req, res, next) => shopController.getAllProducts(req, res, next))
+//router.get('/products/;productId', async (req, res, next) => shopController.getProduct(req, res, next))
+//router.post('/products/:productId', async (req, res, next) => shopController.getProduct(req, res, next))
+//router.get('/cart', async (req, res, next) => shopController.getCart(req, res, next))
+//router.post('/cart', async (req, res, next) => shopController.postCart(req, res, next))
 
-router.post('/cart-delete-item', async (req, res, next) => shopController.cartDeleteitems(req, res, next))
-
-
+// router.post('/cart-delete-item', async (req, res, next) => shopController.cartDeleteitems(req, res, next))
 
 
 
 
 
-module.exports = new ShopController ();
+
+
+module.exports = router;
---
+++ web_shop2.sql
@@ -0,0 +1,105 @@
+-- MySQL dump 10.13  Distrib 8.4.3, for Linux (x86_64)
+--
+-- Host: localhost    Database: web_shop
+-- ------------------------------------------------------
+-- Server version	8.4.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cartItems`
+--
+
+DROP TABLE IF EXISTS `cartItems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cartItems` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `quantity` int DEFAULT NULL,
+  `cartId` int DEFAULT NULL,
+  `productId` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cartId` (`cartId`),
+  KEY `productId` (`productId`),
+  CONSTRAINT `cartItems_ibfk_1` FOREIGN KEY (`cartId`) REFERENCES `carts` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `cartItems_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cartItems`
+--
+
+LOCK TABLES `cartItems` WRITE;
+/*!40000 ALTER TABLE `cartItems` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cartItems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `carts`
+--
+
+DROP TABLE IF EXISTS `carts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carts`
+--
+
+LOCK TABLES `carts` WRITE;
+/*!40000 ALTER TABLE `carts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `description` text NOT NULL,
+  `imageUrl` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'a Book',19.99,'This is an awesome book','https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg'),(2,'Another Book',29.99,'This is a very awesome book','https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-21 12:48:53

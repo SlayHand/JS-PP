0b1fa69f28df9899a4561e99ba050c86f6ce64d9
Carl-Robert <robertm6ts@gmail.com>
2025-01-21 11:57:08 +0200

    Merging all current files

---
+++ controllers/shop.js
@@ -0,0 +1,25 @@
+const db = require ('../utils/db')
+const productModel = require('../models/productModel')
+
+class ShopController{
+    getIndex = async (req, res, next) => {
+        try {
+            const products = await productModel.findAll();
+            console.log(products)
+
+            res.render('shop/index',{
+                prods: products,
+                pageTitle: 'Shop',
+                path: '/',
+                hadProducts: products.length > 0,
+                activeShop: true,
+                productCSS: true
+            }
+        )
+        } catch(error){
+            console.log(error.message)
+        }  
+    }
+} 
+
+module.exports = new ShopController();
